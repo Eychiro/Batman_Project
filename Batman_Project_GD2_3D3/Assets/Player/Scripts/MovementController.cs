e44137ff6c4e020d85d1d73ad2287d35adfa8878
Eychiro <raphaelsymoens@yahoo.fr>
2025-11-18 21:30:55 +0100

    added project

---
+++ Batman_Project_GD2_3D3/Assets/Player/Scripts/MovementController.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform head;
+    public float speed = 5f;
+    public float playerAcceleration = 2f;
+
+    private Rigidbody rb;
+    private Vector3 direction;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        direction = Input.GetAxisRaw("Horizontal") * head.right + Input.GetAxisRaw("Vertical") * head.forward;
+        rb.linearVelocity = Vector3.Lerp(rb.linearVelocity, direction.normalized * playerAcceleration + rb.linearVelocity.y * Vector3.up, playerAcceleration * Time.deltaTime);
+    }
+}
