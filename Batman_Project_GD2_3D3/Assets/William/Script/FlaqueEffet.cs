826e216edc2560d77b3797943dd81e9de2038fef
Wimod <bosc.william31500@gmail.com>
2025-11-25 13:26:32 +0100

    IA v9

---
+++ Batman_Project_GD2_3D3/Assets/William/Script/FlaqueEffet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class FlaqueEffet : MonoBehaviour
+{
+    public float slowFactor = 0.5f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MovementController playerMovement = other.GetComponent<MovementController>();
+            
+            if (playerMovement != null)
+            {
+                playerMovement.speed = playerMovement.speed / 2;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MovementController playerMovement = other.GetComponent<MovementController>();
+            
+            if (playerMovement != null)
+            {
+                playerMovement.speed = playerMovement.speed * 2;
+            }
+        }
+    }
+}
