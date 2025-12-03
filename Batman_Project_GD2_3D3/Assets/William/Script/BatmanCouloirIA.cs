71719e175b592a784412d71a82644943be749bf0
Wimod <bosc.william31500@gmail.com>
2025-12-03 22:03:38 +0100

    modif viteuf

--- Batman_Project_GD2_3D3/Assets/William/Script/BatmanCouloirIA.cs
+++ Batman_Project_GD2_3D3/Assets/William/Script/BatmanCouloirIA.cs
@@ -7,7 +7,7 @@ public class BatmanCouloirIA : MonoBehaviour
     public float mortDistance = 2.5f;
     public float tempsJumpscare = 0.1f; 
     private bool jeuFini = false;
-    private WillCameraController playerScript;
+    private CameraController playerScript;
     public Vector3 positionStandby; //position déterminée dans le niveau, en dehors de tout contact pour éviter collision
     public Transform player;
 
@@ -15,7 +15,7 @@ public class BatmanCouloirIA : MonoBehaviour
 
     void Start()
     {
-        playerScript = player.GetComponent<WillCameraController>();
+        playerScript = player.GetComponent<CameraController>();
         this.transform.position = positionStandby;
 
         GetComponent<Renderer>().enabled = false;
